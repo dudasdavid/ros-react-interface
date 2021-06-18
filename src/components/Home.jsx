20a98b5b365ed87eca8845b9c8852d19814a0c92
david.dudas <david.dudas@outlook.com>
2021-06-18 15:48:37 +0200

    add a test button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Button } from "react-bootstrap"
 
 class Home extends Component {
+    state = {
+        counter: 10,
+    };
+
+    increment_counter() {
+        this.setState({ counter: this.state.counter + 1 })
+        console.log(this.state.counter);
+    };
+
     render() {
-        return (<div><h1>Home</h1></div>);
+        return (<main>
+            <h1 className="text-center mt-3">Control panel</h1>
+            <h5>Counter:<span>{this.state.counter}</span></h5>
+            <Button onClick={() => this.increment_counter()}>Increment</Button>
+        </main>);
     }
 }
 
