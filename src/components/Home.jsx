e7c6461b5ab3f74f8dd2fba1fb8e8202fdcc8ff7
david.dudas <david.dudas@outlook.com>
2021-06-18 21:18:16 +0200

    use grid on main home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import { Button } from "react-bootstrap"
+import Connection from "./Connection.jsx"
+import Teleoperation from './Teleoperation.jsx';
+import { Row, Col, Container, Button } from "react-bootstrap"
 
 class Home extends Component {
     state = {
-        counter: 10,
-    };
-
-    increment_counter() {
-        this.setState({ counter: this.state.counter + 1 })
-        console.log(this.state.counter);
     };
 
     render() {
-        return (<main>
-            <h1 className="text-center mt-3">Control panel</h1>
-            <h5>Counter:<span>{this.state.counter}</span></h5>
-            <Button onClick={() => this.increment_counter()}>Increment</Button>
-        </main>);
+        return (
+
+            <div>
+                <Container>
+                    <h1 className="text-center mt-3">Control panel</h1>
+                    <Row>
+                        <Col>
+                            <Connection />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Teleoperation />
+                        </Col>
+                        <Col>
+                            <h3>MAP</h3>
+                        </Col>
+                    </Row>
+
+                </Container>
+            </div >
+
+        );
     }
 }
 
