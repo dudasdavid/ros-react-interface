d86fd5f9ca36f34f43a4b6b689cee1628a9f3757
david.dudas <david.dudas@outlook.com>
2021-06-18 21:46:12 +0200

    WIP: teleoperation of turtlesim

--- src/components/Teleoperation.jsx
+++ src/components/Teleoperation.jsx
@@ -1,9 +1,85 @@
 import React, { Component } from 'react';
+import { Joystick } from 'react-joystick-component';
+import Config from "../scripts/config.js"
 
 class Teleoperation extends Component {
-    state = {}
+    state = { ros: null, }
+
+    constructor() {
+        super();
+        this.init_connection();
+
+        this.handleMove = this.handleMove.bind(this);
+    }
+
+    init_connection() {
+        this.state.ros = new window.ROSLIB.Ros();
+        console.log(this.state.ros);
+
+        this.state.ros.on("connection", () => {
+            console.log("connection established!");
+            this.setState({ connected: true });
+        });
+
+        this.state.ros.on("close", () => {
+            console.log("connection closed!");
+            this.setState({ connected: false });
+
+            // try to reconnect
+            setTimeout(() => {
+                try {
+                    this.state.ros.connect("ws://" + Config.ROSBRIDGE_SERVER_IP + ":" + Config.ROSBRIDGE_SERVER_PORT);
+                } catch (error) {
+                    console.log("connection error");
+                }
+            }, Config.RECONNECTION_TIME);
+        });
+
+        this.state.ros.on("error", () => {
+            console.log("error!");
+        });
+
+
+        try {
+            this.state.ros.connect("ws://" + Config.ROSBRIDGE_SERVER_IP + ":" + Config.ROSBRIDGE_SERVER_PORT);
+        } catch (error) {
+            console.log("connection error");
+        }
+
+
+    }
+
+    handleMove() {
+        console.log("handle move");
+        var cmd_vel = new window.ROSLIB.Topic({
+            ros: this.state.ros,
+            name: Config.CMD_VEL_TOPIC,
+            messageType: "geometry_msgs/Twist"
+        });
+
+        var twist = new window.ROSLIB.Message({
+            linear: {
+                x: 2,
+                y: 0,
+                z: 0,
+            },
+            angular: {
+                x: 0,
+                y: 0,
+                z: 0,
+            }
+        });
+
+        cmd_vel.publish(twist);
+    }
+    handleStop() {
+        console.log("handle stop")
+    }
+
     render() {
-        return (<h3>Teleop</h3>);
+        return (<div>
+            <Joystick size={100} baseColor="#EEEEEE" stickColor="#BBBBBB" move={this.handleMove} stop={this.handleStop}></Joystick>
+        </div>);
     }
 }
 
--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,6 +2,7 @@ const Config = {
     ROSBRIDGE_SERVER_IP: "localhost",
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIME: 3000,
+    CMD_VEL_TOPIC: "/turtle1/cmd_vel",
 }
 
 export default Config;
