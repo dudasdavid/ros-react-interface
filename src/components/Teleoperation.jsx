86de30578894df8497b7731f26f52fe31dccc753
david.dudas <david.dudas@outlook.com>
2021-06-18 21:18:28 +0200

    add teleoperation draft

---
+++ src/components/Teleoperation.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Teleoperation extends Component {
+    state = {}
+    render() {
+        return (<h3>Teleop</h3>);
+    }
+}
+
+export default Teleoperation;
