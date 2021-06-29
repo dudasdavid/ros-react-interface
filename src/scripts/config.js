0583ba3b4d85be1aab60f90cdd3e211892c8cee2
david.dudas <david.dudas@outlook.com>
2021-06-29 17:30:13 +0200

    use IP address instead of localhost

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,11 +1,12 @@
 const Config = {
-    ROSBRIDGE_SERVER_IP: "localhost",
+    ROSBRIDGE_SERVER_IP: "172.28.177.118",
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIME: 3000,
     //CMD_VEL_TOPIC: "/turtle1/cmd_vel",
     CMD_VEL_TOPIC: "/cmd_vel",
     ODOM_TOPIC: "/odom",
     POSE_TOPIC: "/amcl_pose",
+    CAMERA_TOPIC: "/camera/rgb/image_raw",
 }
 
 export default Config;
